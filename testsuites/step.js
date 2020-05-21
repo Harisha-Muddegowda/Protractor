7234408622130c5ea7910559996052c8ee5a77f1
Harisha-Muddegowda <59262867+Harisha-Muddegowda@users.noreply.github.com>
2020-05-21 20:13:23 +0530

    Update step.js

--- testsuites/step.js
+++ testsuites/step.js
@@ -1,16 +1,14 @@
 var { Given } = require('cucumber');
 
 Given('I navigates to google', async function () {
-  browser.driver.sleep(10000);
+  browser.waitForAngularEnabled(false);
   await browser.get('https://www.google.com/');
 });
 
 Given('searches for {string}', async function (searchValue) {
-  browser.driver.sleep(10000);
   await element(by.css('input[role="combobox"]')).sendKeys(searchValue)
 });
 
 Given('I should see {string}', async function (expectedValue) {
-  browser.driver.sleep(5000);
   expect(expectedValue).to.equal('fail')
 });
